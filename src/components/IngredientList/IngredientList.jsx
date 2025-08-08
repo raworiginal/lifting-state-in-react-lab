7b50205febe1dcdf03c43fde87fc76ef6206536d
raworiginal <randall.a.wolfe@gmail.com>
2025-08-08 15:00:12 -0700

    completed list of ingredients on the left side of the screen

---
+++ src/components/IngredientList/IngredientList.jsx
@@ -0,0 +1,18 @@
+// src/components/IngredientList/IngredientList.jsx
+
+const IngredientList = (props) => {
+	return (
+		<ul>
+			{props.availableIngredients.map((ingredient, index) => {
+				return (
+					<li key={index} style={{ backgroundColor: ingredient.color }}>
+						{ingredient.name}
+						<button>+</button>
+					</li>
+				);
+			})}
+		</ul>
+	);
+};
+
+export default IngredientList;
